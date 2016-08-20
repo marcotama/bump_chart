2b16e4358841dd2306c0729efc39d70a53e4b33b
Marco Tamassia <tamassia.marco@gmail.com>
2016-08-20 13:59:27 +1000

    Use OrderedDict to preserve legend entries order

--- create_bump_chart.py
+++ create_bump_chart.py
@@ -1,19 +1,13 @@
 from bump_chart import plot_bump_chart
 import pandas as pd
+from collections import OrderedDict
 
-# data = {
-#     'Aggressive': [1, 2, 3, 4, 5, 6],
-#     'Conservative': [1, 6, 2, 7, 3, 4],
-#     'Marco': [1, 2, 8, 6, 9, 10],
-#     'William': [1, 2, 6, 11, 8, 12]
-# }
-
-data = {
-    'Aggressive':   [60, 50, 40, 30, 20, 10, 00, 00, 00, 00, 00, 00],
-    'Conservative': [60, 40, 20, 10, 00, 50, 30, 00, 00, 00, 00, 00],
-    'Marco':        [60, 50, 00, 00, 00, 30, 00, 40, 20, 10, 00, 00],
-    'William':      [60, 50, 00, 00, 00, 40, 00, 20, 00, 00, 30, 10]
-}
+data = OrderedDict((
+    ('Aggressive',   [60, 50, 40, 30, 20, 10, 00, 00, 00, 00, 00, 00]),
+    ('Conservative', [60, 40, 20, 10, 00, 50, 30, 00, 00, 00, 00, 00]),
+    ('Marco',        [60, 50, 00, 00, 00, 30, 00, 40, 20, 10, 00, 00]),
+    ('William',      [60, 50, 00, 00, 00, 40, 00, 20, 00, 00, 30, 10])
+))
 goals = [
     '3, 1, 3, None',
     '3, 2, 3, None',
@@ -28,6 +22,7 @@ goals = [
     '3, 1, 3, 3',
     '2, 1, 3, None'
 ]
+
 colors = [
     'green',
     'darkred',
@@ -49,8 +44,8 @@ data = data.set_index('goals')
 
 plot_bump_chart(
     data,
-    colors={g: c for g, c in zip(goals, colors)},
+    title="Ranking of goals",
+    colors=OrderedDict((g, c) for g, c in zip(goals, colors)),
     savename=['bumps.png', 'bumps.pdf'],
     dpi=300
 )
-
